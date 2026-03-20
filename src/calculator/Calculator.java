ebfb2ea239ffb387c8195d9b5dc40700c8d2e6e5
sirisiyesiri <shil690321@naver.com>
2026-03-20 20:59:45 +0900

    index 범위 및 사이즈 체크 & 0나누기 문제 throws처리로 해결

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -6,11 +6,12 @@ public class Calculator {
     // 속성
     private int result;
     private ArrayList<String> arraylist = new ArrayList<>();
+
     
     // 생성자
     
     // 기능
-    public void calculate(int num1, int num2, char operation) {
+    public int calculate(int num1, int num2, char operation) throws ArithmeticException{
         switch(operation) {
             case '+':
                 result = num1 + num2;
@@ -22,21 +23,24 @@ public class Calculator {
                 result = num1 * num2;
                 break;
             case '/':
-                try{
-                    result = num1 / num2;
-                } catch(RuntimeException e) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                if(num2 == 0) {
+                    arraylist.add((arraylist.size()+1) + ". " + num1 + " " + operation + " " + num2 + " = 계산 불가");
+                    throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    // 처음엔 exception이 나지 않기 위해서 여기서 try-catch를 하고, result = 0;을 return하게 코드를 짰었음
+                    // 근데 그러니까 main에서 계산 불가임에도 불구하고, '계산 결과 : 0'으로 출력되며, 원하는 출력이 나오지 않게 됨
+                    // 튜터님께 여쭤보니 차라리 exception을 발생시키고 throws를 이용해서 main에서 exception을 처리하라는 답을 주심
                 }
+                result = num1 / num2;
                 break;
             default:
                 System.out.println("지원하지 않는 연산 기능입니다.");
+                arraylist.add((arraylist.size()+1) + ". 지원하지 않는 연산");
+                return result = 0;
         }
-        arraylist.add(num1 + " " + operation + " " + num2 + " = " + result);
-    }
-
-    public int getResult() {    // 계산 결과
+        arraylist.add((arraylist.size()+1) + ". " + num1 + " " + operation + " " + num2 + " = " + result);
         return result;
     }
+
     public void getArrayList() {   // 계산 결과 전체 조회
         for(String list : arraylist) {
             System.out.println(list);
@@ -54,9 +58,7 @@ public class Calculator {
     public void removeArrayList() { // 가장 먼저 저장된 계산 결과 삭제
         arraylist.remove(0);
     }
-    
-    
-    
-
-
+    public int listSize() {
+        return arraylist.size();
+    }
 }
--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,8 +10,9 @@ public class Main {
         int number1 = 0;
         int number2 = 0;
         char operation = ' ';
+        int result = 0;
         int index;
-        boolean flag = true;
+        int indexCount;
 
         System.out.println("=== Java 계산기 ===");
 
@@ -23,10 +25,6 @@ public class Main {
             System.out.println("4. 계산 결과 수정");
             System.out.println("5. 계산 결과 삭제");
 
-            System.out.println();
-            System.out.print("계산을 종료하시겠습니까? (계속 -> y, 종료 -> exit) : ");
-            String endAnswer = scanner.nextLine();
-            if(endAnswer.equals("exit")) break;
 
             System.out.println();
             System.out.print("선택 : ");
@@ -37,41 +35,56 @@ public class Main {
             switch(choice) {
                 case 1:
                     while(true) {
-                        try {   // 잘못된 자료형의 입력을 받을 시 예외처리
-                            System.out.print("첫 번째 수를 입력해 주세요. : ");
-                            number1 = scanner.nextInt();
-                        } catch(RuntimeException e) {
-                            System.out.println("잘못된 입력입니다. 정수를 입력해주세요.");
-                        }
-                        try {
-                            System.out.print("두 번째 수를 입력해 주세요. : ");
-                            number2 = scanner.nextInt();
-                        } catch (RuntimeException e) {
-                            System.out.println("잘못된 입력입니다. 정수를 입력해주세요.");
-                        }
-                        scanner.nextLine(); // scanner 비우기
-                        try {
-                            System.out.print("계산 기호를 입력해주세요.(+, -, *, /) : ");
-                            operation = scanner.nextLine().charAt(0);  // 입력한 문자열의 맨 첫 번째 글자 operation 변수에 입력
-                        } catch (RuntimeException e) {
-                            System.out.println("잘못된 입력입니다. 계산 기호를 입력해주세요.");
+                        boolean flag = true;
+                        while(flag) {
+                            try {   // 잘못된 자료형의 입력을 받을 시 예외처리
+                                System.out.print("첫 번째 수를 입력해 주세요. : ");
+                                number1 = scanner.nextInt();
+                            } catch(InputMismatchException e) {
+                                System.out.println("잘못된 입력입니다. 정수를 입력해주세요.");
+                                scanner.nextLine();
+                                // scanner비우기 -> 비우지 않으면 number1 = scanner.nextInt() 구문에 도달할 때 마다 예외 발생
+                                continue;
+                            }
+                            try {
+                                System.out.print("두 번째 수를 입력해 주세요. : ");
+                                number2 = scanner.nextInt();
+                            } catch (InputMismatchException e) {
+                                System.out.println("잘못된 입력입니다. 정수를 입력해주세요.");
+                                scanner.nextLine();
+                                continue;
+                            }
+                            scanner.nextLine(); // scanner 비우기
+                            try {
+                                System.out.print("계산 기호를 입력해주세요.(+, -, *, /) : ");
+                                operation = scanner.nextLine().charAt(0);  // 입력한 문자열의 맨 첫 번째 글자 operation 변수에 입력
+                            } catch (RuntimeException e) {
+                                System.out.println("잘못된 입력입니다. 계산 기호를 입력해주세요.");
+                                scanner.nextLine();
+                                continue;
+                            }
+                            flag = false;
                         }
 
-
-                        calculator.calculate(number1, number2, operation); // 사칙연산
-                        int result = calculator.getResult();
-                        System.out.println("계산 결과 : " + result);
+                        try{
+                            result = calculator.calculate(number1, number2, operation);
+                            System.out.println("계산 결과 : " + result);
+                        } catch (ArithmeticException e) {   // ArithmeticException발생 시 계산 결과 출력X, 오류 메세지 출력
+                            System.out.println(e.getMessage());
+                        }
 
                         while(true) {
-                            scanner.nextLine(); // scanner 비우기
+//                            scanner.nextLine(); // scanner 비우기
                             System.out.print("기존 계산 결과를 이어서 계속하시겠습니까? (y/n) : ");
                             String continueAnswer = scanner.nextLine();
                             if(continueAnswer.equals("y")) {
                                 try {
                                     System.out.print("두 번째 수를 입력해 주세요. : ");
                                     number2 = scanner.nextInt();
-                                } catch (RuntimeException e) {
+                                } catch (InputMismatchException e) {
                                     System.out.println("잘못된 입력입니다. 정수를 입력해주세요.");
+                                    scanner.nextLine(); // scanner 비우기
+                                    continue;
                                 }
                                 scanner.nextLine(); // scanner 비우기
                                 try {
@@ -79,10 +92,16 @@ public class Main {
                                     operation = scanner.nextLine().charAt(0);  // 입력한 문자열의 맨 첫 번째 글자 operation 변수에 입력
                                 } catch (RuntimeException e) {
                                     System.out.println("잘못된 입력입니다. 계산 기호를 입력해주세요.");
+                                    scanner.nextLine();  // scanner 비우기
+                                    continue;
                                 }
-                                calculator.calculate(result, number2, operation);
-                                result = calculator.getResult();
-                                System.out.println("계산 결과 : " + result);
+
+                                try{
+                                    result = calculator.calculate(result, number2, operation);
+                                    System.out.println("계산 결과 : " + result);
+                                } catch (ArithmeticException e) {   // ArithmeticException발생 시 계산 결과 출력X, 오류 메세지 출력
+                                    System.out.println(e.getMessage());
+                                }
                             }
                             else break;
                         }
@@ -94,30 +113,60 @@ public class Main {
                     calculator.getArrayList();
                     break;
                 case 3:
-                    System.out.print("조회 하고 싶은 계산 결과 번호 : ");
-                    index = scanner.nextInt();
-                    calculator.getSingleArrayList(index - 1);
+                    indexCount = calculator.listSize();
+                    if(indexCount != 0) {
+                        System.out.print("조회 하고 싶은 계산 결과 번호 (1 ~ " + indexCount + ") : ");
+                        index = scanner.nextInt();
+                        if(index < 1 || index > indexCount) {
+                            System.out.println("잘못된 번호입니다.");
+                            break;
+                        }
+                        calculator.getSingleArrayList(index - 1);
+                    } else {
+                        System.out.println("조회할 계산 결과가 존재하지 않습니다.");
+                    }
                     break;
                 case 4:
-                    System.out.print("수정하고 싶은 계산 결과 번호 : ");
-                    index = scanner.nextInt();
-                    scanner.nextLine(); // scanner 비우기
-                    System.out.println("수정 값 : ");
-                    String correction = scanner.nextLine();
-                    calculator.setArrayList(index-1, correction);
-                    System.out.println(index + "번 째 계산 결과가 수정되었습니다.");
+                    indexCount = calculator.listSize();
+                    if(indexCount != 0) {
+                        System.out.print("수정하고 싶은 계산 결과 번호 (1 ~ " + indexCount + ") : ");
+                        index = scanner.nextInt();
+                        scanner.nextLine(); // scanner 비우기
+                        if(index < 1 || index > indexCount) {
+                            System.out.println("잘못된 번호입니다.");
+                            break;
+                        }
+                        System.out.println("수정 값 : ");
+                        String correction = scanner.nextLine();
+                        calculator.setArrayList(index-1, correction);
+                        System.out.println(index + "번 째 계산 결과가 수정되었습니다.");
+                    } else {
+                        System.out.println("수정할 계산 결과가 존재하지 않습니다.");
+                    }
                     break;
                 case 5:
-                    calculator.removeArrayList();
-                    System.out.println("1번 째 계산 결과가 삭제되었습니다.");
+                    if(calculator.listSize() != 0) {
+                        calculator.removeArrayList();
+                        System.out.println("1번 째 계산 결과가 삭제되었습니다.");
+                    } else {
+                        System.out.println("삭제할 계산 결과가 존재하지 않습니다.");
+                    }
                     break;
                 default:
                     System.out.println("잘못된 입력입니다.");
             }
+            System.out.println();
+            System.out.print("계산을 계속하시겠습니까? (계속 -> y, 종료 -> exit) : ");
+            String endAnswer = scanner.nextLine();
+            if(endAnswer.equals("exit")) break;
         }
 
         System.out.println("계산기를 종료합니다.");
 
         scanner.close();
+
+
+        // 조금 더 객체 지향적으로 한다면 scannermanager class를 만들어서 원하는 scanner 기능을 사용
+        // ex) 해당 scanner 매서드는 어떤 exception을 처리하는지 등
     }
 }
